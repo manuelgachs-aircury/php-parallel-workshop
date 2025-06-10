43faf12677fe051d17566ef2f262a00c0a268571
manuelgachs-aircury <manuel.gachs@aircury.com>
2025-06-10 17:36:56 +0200

    Add wait time and card parsing to Scryfall

--- src/Scryfall.php
+++ src/Scryfall.php
@@ -6,18 +6,30 @@ class Scryfall
     private const string RANDOM_CARD_URL = self::BASE_URL.'random';
 
     private array $headers;
+    private int $sleepTime;
 
-    public function __construct(array $headers = null) {
+    public function __construct(int $sleepTime = 100000, array $headers = null) {
         if (isset($headers)) {
             $this->headers = $headers;
         } else {
             $this->headers = ['Accept: application/json', 'User-Agent: parallel-php-workshop'];
         }
+
+        // From Scryfall API docs
+        // We kindly ask that you insert 50 – 100 milliseconds of delay between the requests
+        if ($sleepTime <= 100000) {
+            $this->sleepTime = 100000;
+        } else {
+            $this->sleepTime = $sleepTime;
+        }
+
     }
 
-    public function fetch_random(): array
+    private function fetch(string $url): string
     {
-        $ch = curl_init(self::RANDOM_CARD_URL);
+        usleep($this->sleepTime);
+
+        $ch = curl_init($url);
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return response as a string
         curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers); // Set headers
@@ -30,10 +42,25 @@ class Scryfall
             throw new Exception("cURL Error: " . $error);
         }
 
-        // Close cURL session
-        curl_close($ch);
+        return $response;
+    }
 
-        // Decode the JSON response and return as an array
-        return json_decode($response, true);
+    private static function parse_card(array $card): array
+    {
+        $parsed_card = [];
+        $parsed_card['scryfall_id'] =  $card['id'] ?? '';
+        $parsed_card['name'] =  $card['name'] ?? '';
+        $parsed_card['set_code'] =  $card['set'] ?? '';
+        $parsed_card['collector_number'] =  $card['collector_number'] ?? '';
+
+        return $parsed_card;
+    }
+
+    public function fetch_random(): array
+    {
+        $response = $this->fetch(self::RANDOM_CARD_URL);
+        $raw_card = json_decode($response, true);
+
+        return self::parse_card($raw_card);
     }
 }
